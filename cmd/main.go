298c40241436e529d41259f2fcac5beed5b04dda
daikw <daikw@yak-shaver.com>
2024-06-11 15:27:18 +0900

    :recycle: separate actions functions from main.go

--- cmd/main.go
+++ cmd/main.go
@@ -6,91 +6,15 @@ import (
 	"os"
 
 	awslogin "github.com/Photosynth-inc/aws-google-login"
-	"github.com/manifoldco/promptui"
 	"github.com/rs/zerolog"
 	"github.com/urfave/cli/v3"
 )
-
-func handler(ctx context.Context, c *cli.Command) (err error) {
-	g, err := awslogin.LoadConfig(awslogin.AWSConfigPath(), c.String("profile"))
-	if err != nil {
-		return err
-	}
-
-	// Override the configuration if the flags are set
-	{
-		if c.String("sp-id") != "" {
-			g.Google.GoogleSPID = c.String("sp-id")
-		}
-		if c.String("idp-id") != "" {
-			g.Google.GoogleIDPID = c.String("idp-id")
-		}
-		if c.String("role-arn") != "" {
-			g.Google.RoleARN = c.String("role-arn")
-		}
-		if c.Int("duration-seconds") != 0 {
-			g.Google.Duration = c.Int("duration-seconds")
-		}
-	}
-
-	authnRequest, err := g.Login(&awslogin.LoginOptions{
-		Verbose:        zerolog.GlobalLevel() < zerolog.WarnLevel,
-		BrowserTimeout: c.Float("browser-timeout"),
-	})
-	if err != nil {
-		return err
-	}
-
-	amz, err := awslogin.NewAWSConfig(authnRequest, g)
-	if err != nil {
-		return err
-	}
-
-	var role *awslogin.Role
-	if c.Bool("select-role-interactivelly") {
-		roles, err := amz.ResolveAliases(ctx)
-		if err != nil {
-			return err
-		}
-		prompt := promptui.Select{
-			Label: "Select AWS Role",
-			Items: roles,
-			Size:  10,
-		}
-		index, _, err := prompt.Run()
-		if err != nil {
-			return fmt.Errorf("prompt failed %v", err)
-		}
-		role = roles[index]
-	} else {
-		role, err = amz.ResolveRole(g.Google.RoleARN)
-		if err != nil {
-			return err
-		}
-	}
-
-	creds, err := amz.AssumeRole(ctx, role)
-	if err != nil {
-		return err
-	}
-
-	awsCreds := &awslogin.AWSCredentials{
-		Profile:     g.Profile,
-		Credentials: creds,
-	}
-
-	if err := awsCreds.SaveTo(awslogin.AWSCredPath()); err != nil {
-		return err
-	}
-	fmt.Println("Temporary AWS credentials have been saved to", awslogin.AWSCredPath())
-	return nil
-}
 
 func main() {
 	app := &cli.Command{
 		Name:   "aws-google-login",
 		Usage:  "Acquire temporary AWS credentials via Google SSO (SAML v2)",
-		Action: handler,
+		Action: handleMain,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "profile",
